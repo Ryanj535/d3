8b556d41ca7b6fda187c89dfefce3b096c7b29c6
Ryan J <rijoseph89@gmail.com>
2019-05-13 15:26:24 -0400

    added abbrviations to bubble chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 // svg dimensions
-var svgWidth = 960;
+var svgWidth = 800;
 var svgHeight = 660;
 
 var chartMargin = {
   top: 30,
   right: 30,
-  bottom: 30,
-  left: 30
+  bottom: 50,
+  left: 50
 };
 
 // Dimensions of chart area
@@ -27,7 +27,6 @@ d3.csv('./assets/data/data.csv').then(function(healthData) {
     data.healthcare = +data.healthcare;
     data.poverty = +data.poverty
     });
-
     var xLinearScale = d3.scaleLinear()
      .domain([d3.min(healthData, d => d.poverty) - 1.25, d3.max(healthData, d => d.poverty)])
      .range([0, chartWidth]);
@@ -55,20 +54,24 @@ d3.csv('./assets/data/data.csv').then(function(healthData) {
       .attr("r", "10")
       .attr("fill", "blue")
       .attr("opacity", ".5");
-      
-     var textGroup = chartGroup.selectAll("p")
+
+     var textGroup = chartGroup.selectAll("text")
        .data(healthData)
        .enter()
-       .append("p")
-       .attr("left", d => xLinearScale(d.poverty))
-       .attr("top", d => yLinearScale(d.healthcare))
-       .attr("p", d => d.abbr)
+       .append("text")
+       .text(d => d.abbr)
+       .attr("dx", d => xLinearScale(d.poverty))
+       .attr("dy", d => yLinearScale(d.healthcare) +3)
+       .attr("font-family", "sans-serif")
+       .attr("font-size", "8px")
+       .attr("fill", "white")
+       .attr("text-anchor", "middle");
 
     var toolTip = d3.tip()
       .attr("class", "d3-tip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.state}<br>poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
+        return (`${d.state}<br> In Poverty: ${d.poverty}%<br> Lacks Healthcare: ${d.healthcare}%`);
       });
 
     chartGroup.call(toolTip);
@@ -81,17 +84,26 @@ d3.csv('./assets/data/data.csv').then(function(healthData) {
         toolTip.hide(data);
       });
 
+      textGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+      })
+        // onmouseout event
+        .on("mouseout", function(data, index) {
+          toolTip.hide(data);
+        });
+
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - chartMargin.left + 40)
+      .attr("y", 0 - chartMargin.left + 5)
       .attr("x", 0 - (chartHeight / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Healthcare");
+      .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight +40})`)
       .attr("class", "axisText")
-      .text("Poverty");
+      .text("In Poverty (%)");
+
   });
